10d15a9734547022eac7ae04d45c741258e52610
mouadmaa <maaroufimouad00@gmail.com>
2021-02-22 22:55:44 +0100

    added search header

--- src/layout/header/header.component.tsx
+++ src/layout/header/header.component.tsx
@@ -1,22 +1,38 @@
 import React, { FC, useEffect, useState } from 'react'
 import Link from 'next/link'
+import 'twin.macro'
 
 import { StyledHeader } from './header.styles'
 
 const Header: FC = () => {
-	const [ showBG, setShowBG ] = useState(false)
+	const [ showHeaderBG, setHeaderShowBG ] = useState(false)
+	const [ showSearch, setShowSearch ] = useState(false)
 
 	useEffect(() => {
-		window.addEventListener('scroll', () => {
-			setShowBG(window.scrollY >= 1)
-		})
+		window.addEventListener('scroll', onScrollShowBG)
+		return () => window.removeEventListener('scroll', onScrollShowBG)
 	}, [])
 
+	const onScrollShowBG = () => {
+		setHeaderShowBG(window.scrollY >= 1)
+		setShowSearch(window.scrollY >= 160)
+	}
+
 	return (
-		<StyledHeader showBG={showBG}>
+		<StyledHeader showHeaderBG={showHeaderBG} showSearch={showSearch}>
 			<Link href='/'>
 				<a>Estates</a>
 			</Link>
+
+			<div>
+				<span>Commencez votre recherche</span>
+				<button>
+					<svg viewBox='0 0 24 24' fill='none' xmlns='http://www.w3.org/2000/svg'>
+						<circle cx='11.767' cy='11.767' r='8.989' strokeLinecap='round' strokeLinejoin='round' />
+						<path d='M18.018 18.485L21.542 22' strokeLinecap='round' strokeLinejoin='round' />
+					</svg>
+				</button>
+			</div>
 
 			<nav>
 				<Link href='/'>
@@ -26,9 +42,6 @@ const Header: FC = () => {
 				<button>
 					<svg viewBox='0 0 24 24' fill='none' xmlns='http://www.w3.org/2000/svg' aria-hidden='true'>
 						<path d='M21 18H3v-2h18v2zm0-5H3v-2h18v2zm0-5H3V6h18v2z' />
-					</svg>
-					<svg viewBox='0 0 32 32' xmlns='http://www.w3.org/2000/svg' aria-hidden='true'>
-						<path d='M16 .7C7.563.7.7 7.563.7 16S7.563 31.3 16 31.3 31.3 24.437 31.3 16 24.437.7 16 .7zm0 28c-4.021 0-7.605-1.884-9.933-4.81a12.425 12.425 0 016.451-4.4A6.507 6.507 0 019.5 14c0-3.584 2.916-6.5 6.5-6.5s6.5 2.916 6.5 6.5a6.513 6.513 0 01-3.019 5.491 12.42 12.42 0 016.452 4.4C23.605 26.816 20.021 28.7 16 28.7z' />
 					</svg>
 				</button>
 			</nav>
--- src/layout/header/header.styles.ts
+++ src/layout/header/header.styles.ts
@@ -1,27 +1,69 @@
 import tw, { css, styled } from 'twin.macro'
 
-export const StyledHeader = styled.header(({ showBG }: { showBG: boolean }) => [
-	tw`sticky top-0 z-40 flex items-center justify-between w-full h-24 px-4 md:px-8 text-gray-50 transition-all ease-out duration-300`,
-	css`
-		> a {
-			${tw`text-xl font-semibold tracking-widest transition-colors`};
-		}
-
-		nav {
-			${tw`flex items-center space-x-2`};
-
-			a {
-				${tw`hidden sm:inline-block tracking-wider px-5 py-3 rounded-3xl transition-colors duration-300 hover:bg-gray-50 hover:bg-opacity-5`};
+export const StyledHeader = styled.header(
+	({ showHeaderBG, showSearch }: { showHeaderBG: boolean; showSearch: boolean }) => [
+		tw`sticky top-0 z-40 flex items-center justify-between w-full h-24 px-4 md:px-8 text-gray-50 transition-all ease-out duration-300`,
+		css`
+			> a {
+				${tw`text-xl font-semibold tracking-widest transition-colors`};
 			}
 
-			button {
-				${tw`flex items-center justify-between p-2 space-x-2 text-gray-600 rounded-full bg-gray-50 shadow-sm transition-shadow duration-300 hover:shadow-md focus:outline-none`};
+			> div {
+				${tw`hidden sm:flex items-center p-1 space-x-6 border-2 rounded-2xl cursor-pointer opacity-0 transform scale-50 translate-y-6 origin-center transition duration-300`};
 
-				svg {
-					${tw`w-6 h-6 fill-current`};
+				span {
+					${tw`text-sm ml-2`};
+				}
+
+				button {
+					${tw`flex items-center justify-center p-2 space-x-3 rounded-xl text-gray-50 bg-primary-default focus:outline-none`};
+
+					svg {
+						${tw`w-4 h-4 stroke-current stroke-2`};
+					}
 				}
 			}
-		}
-	`,
-	showBG && [ tw`h-16 text-gray-700 bg-white shadow-sm` ],
-])
+
+			nav {
+				${tw`flex items-center space-x-2`};
+
+				a {
+					${tw`hidden sm:inline-block tracking-wider px-5 py-2 rounded-2xl transition-colors duration-300 hover:bg-gray-50 hover:bg-opacity-5`};
+				}
+
+				button {
+					${tw`flex items-center justify-between p-1.5 space-x-2 text-gray-600 rounded-2xl bg-gray-50 shadow-sm focus:outline-none`};
+
+					svg {
+						${tw`w-6 h-6 fill-current`};
+					}
+				}
+			}
+		`,
+		showHeaderBG && [
+			tw`h-16 text-gray-700 bg-white shadow-sm`,
+			css`
+				> a {
+					${tw`text-primary-default`};
+				}
+
+				nav {
+					a {
+						${tw`hover:bg-gray-100`};
+					}
+
+					button {
+						${tw`p-1 border-2 border-gray-200`};
+					}
+				}
+			`,
+		],
+		showSearch && [
+			css`
+				> div {
+					${tw`scale-100 translate-y-0 opacity-100`};
+				}
+			`,
+		],
+	],
+)
